a8d9509c1b9cbb99cf7f5d8ea768d8a661e17e1b
Hugh Lee <hammerha@gmail.com>
2016-01-05 15:26:34 +0900

    Add nested class test

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -1,3 +1,4 @@
+import lombok.AllArgsConstructor;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,5 +12,31 @@ public class ClassTest {
     public void classPathTest() throws Exception {
         String classPath = this.getClass().getClassLoader().getResource("").getPath();
         System.out.println(classPath);
+    }
+    
+    static class Outer {
+        @AllArgsConstructor
+        static class StaticInner {
+            public final int no;
+        }
+
+        @AllArgsConstructor
+        class Inner {
+            public final int no;
+        }
+    }
+
+    /**
+     * http://docs.oracle.com/javase/tutorial/java/javaOO/nested.html
+     * http://stackoverflow.com/questions/70324/java-inner-class-and-static-nested-class
+     */
+    @Test
+    public void nestedClassTest() throws Exception {
+        Outer.StaticInner staticInner = new Outer.StaticInner(1);
+        assertThat(staticInner.no, is(1));
+
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner(2);
+        assertThat(inner.no, is(2));
     }
 }
