e22c0243f652382c533e1fe940e152770fa40c42
Hugh Lee <hammerha@gmail.com>
2016-02-04 17:25:02 +0900

    Add equals tests

--- src/test/java/ObjectsTest.java
+++ src/test/java/ObjectsTest.java
@@ -1,20 +1,55 @@
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author hugh
  */
 public class ObjectsTest {
 
-    @EqualsAndHashCode
-    @RequiredArgsConstructor
-    public static class Person {
-        public final int age;
-        public final String name;
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Person {
+        String name;
+        int age;
+
+        @Override
+        public boolean equals(Object obj) {
+            return name.equals(((Person)obj).name);
+        }
+
+        @Override
+        public int hashCode() {
+            return age;
+        }
+    }
+
+    static class Child extends Person {
+
+        public Child(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this.age == ((Person)obj).age;
+        }
+    }
+
+    @Test
+    public void equalsTest() throws Exception {
+        Person person = new Person("SD", 34);
+        Child child = new Child("SD", 14);
+
+        assertTrue(person.equals(child));
+        assertFalse(child.equals(person));
     }
 
     /**
@@ -23,8 +58,8 @@ public class ObjectsTest {
      */
     @Test
     public void hashCodeAndIdentityTest() throws Exception {
-        Person p1 = new Person(10, "Tom");
-        Person p2 = new Person(10, "Tom");
+        Person p1 = new Person("Tom", 10);
+        Person p2 = new Person("Tom", 10);
 
         assertThat(p1.hashCode(), is(p2.hashCode()));
         assertThat(System.identityHashCode(p1), is(not(System.identityHashCode(p2))));
