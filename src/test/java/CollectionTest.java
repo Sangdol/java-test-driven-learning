cf2114c1f49dd40eb863eb6388874a9bd4d28307
Hugh Lee <hammerha@gmail.com>
2016-02-06 22:44:04 +0900

    Add map and set tests

--- src/test/java/CollectionTest.java
+++ src/test/java/CollectionTest.java
@@ -1,10 +1,10 @@
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -15,19 +15,88 @@ import static org.junit.Assert.assertThat;
 public class CollectionTest {
     
     @Test
-    public void mapCreation() throws Exception {
+    public void setIterationTest() throws Exception {
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+
+        for (Integer i : set) {
+            assertThat(i, is(greaterThan(0)));
+        }
+    }
+
+    @Test
+    public void setTest() throws Exception {
+        Set<Integer> set = new HashSet<>();
+
+        assertThat(set.add(1), is(true));
+        assertThat(set.add(1), is(false));
+        assertThat(set.contains(1), is(true));
+        assertThat(set.contains(2), is(false));
+    }
+    
+    @Test
+    public void mapCreationTest() throws Exception {
         Map<Integer, String> map = Collections.singletonMap(1, "one");
 
         assertThat(map.get(1), is("one"));
     }
     
     @Test
-    public void mapTest() throws Exception {
-        Map<Integer, String> immutableMap = ImmutableMap.of(1, "one", 2, "two");
+    public void replaceTest() throws Exception {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
 
-        assertThat(immutableMap.get(1), is("one"));
-        assertThat(immutableMap.get(3), is(nullValue()));
-        assertThat(immutableMap.getOrDefault(3, "three"), is("three"));
+        assertThat(map.replace(1, "1"), is("one"));
+        assertThat(map.replace(2, "2"), is(nullValue()));
+
+        assertThat(map.replace(1, "1", "!"), is(true));
+        assertThat(map.replace(1, "1", "!"), is(false));
+
+        map.replaceAll((key, value) -> key + value);
+        assertThat(map.get(1), is("1!"));
+    }
+
+    @Test
+    public void mapIterationTest() throws Exception {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            assertThat(entry.getKey(), is(greaterThan(0)));
+            assertThat(entry.getValue().length(), is(3));
+        }
+    }
+
+    @Test
+    public void mapPutTest() throws Exception {
+        Map<Integer, String> map = new HashMap<>();
+
+        assertThat(map.put(1, "one"), is(nullValue()));
+        assertThat(map.put(1, "1"), is("one"));
+        
+        assertThat(map.putIfAbsent(1, "!"), is("1"));
+        assertThat(map.get(1), is("1"));
+
+        Map<Integer, String> map2 = new HashMap<>();
+        map2.put(1, "one");
+        map2.put(2, "two");
+
+        map.putAll(map2);
+        assertThat(map.size(), is(2));
+        assertThat(map.get(1), is("one"));
+    }
+    
+    @Test
+    public void mapGetTest() throws Exception {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+
+        assertThat(map.get(1), is("one"));
+        assertThat(map.get(3), is(nullValue()));
+        assertThat(map.getOrDefault(3, "three"), is("three"));
     }
 
     @Test
