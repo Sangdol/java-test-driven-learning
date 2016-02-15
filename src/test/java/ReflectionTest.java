07f21372f75c68e4cea4968d178959938752e7c9
Hugh Lee <hammerha@gmail.com>
2016-02-15 20:14:38 +0900

    Add getting annotation tests

--- src/test/java/ReflectionTest.java
+++ src/test/java/ReflectionTest.java
@@ -9,6 +9,16 @@ import java.lang.reflect.Method;
  */
 public class ReflectionTest {
 
+    /**
+     * http://stackoverflow.com/questions/20192552/get-value-of-a-parameter-of-an-annotation-in-java
+     */
+    @Test
+    public void annotationTest() throws Exception {
+        Method method = ReflectionTest.class.getMethod("annotationTest");
+        Test test = method.getAnnotation(Test.class);
+        assertThat(test.timeout(), is(0L));
+    }
+
     @Test
     public void getMethodTest() throws Exception {
         Method concat = String.class.getMethod("concat", String.class);
