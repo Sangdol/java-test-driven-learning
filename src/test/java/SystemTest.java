0265fa5241a8cb4fe2b1aaac18ca565c983d8d87
Hugh Lee <hammerha@gmail.com>
2016-04-09 14:01:22 +0900

    Add System in/out tests

--- src/test/java/SystemTest.java
+++ src/test/java/SystemTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.io.*;
+import java.util.Scanner;
+
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
@@ -15,5 +18,26 @@ public class SystemTest {
         assertThat(System.nanoTime(), is(greaterThan(824_674_741_842_607L)));
         assertThat(System.nanoTime() / 100_000, is(greaterThan(Long.valueOf(Integer.MAX_VALUE))));
         assertThat(System.nanoTime() / 1_000_000, is(lessThan(Long.valueOf(Integer.MAX_VALUE))));
+    }
+
+    /**
+     * http://stackoverflow.com/questions/13329282/test-java-programs-that-read-from-stdin-and-write-to-stdout
+     * http://stackoverflow.com/questions/1647907/junit-how-to-simulate-system-in-testing
+     */
+    @Test
+    public void systemInOutReplaceTest() throws Exception {
+        // Set In
+        String hi = "hi";
+        System.setIn(new ByteArrayInputStream(hi.getBytes()));
+        Scanner scanner = new Scanner(System.in);
+
+        // Set Out
+        OutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        // Test
+        System.out.print(scanner.next());
+        assertThat(outputStream.toString(), is("hi"));
     }
 }
