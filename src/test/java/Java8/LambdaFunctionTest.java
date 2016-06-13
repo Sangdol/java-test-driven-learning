79f027bc3b7ec262952d050ae552ff65967bf672
Hugh Lee <hammerha@gmail.com>
2016-06-13 12:29:23 +0900

    Add LambdaFunctionTest

---
+++ src/test/java/Java8/LambdaFunctionTest.java
@@ -0,0 +1,60 @@
+package Java8;
+
+import org.junit.Test;
+
+import java.util.function.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author hugh
+ */
+public class LambdaFunctionTest {
+    
+    @Test
+    public void functionTest() throws Exception {
+        Function<Integer, Double> toDouble = Double::valueOf;
+        assertThat(toDouble.apply(1), is(1d));
+        
+        Function<Double, Double> tenTimes = (d) -> 10 * d;
+        assertThat(toDouble.andThen(tenTimes).apply(2), is(20d));
+    }
+    
+    @Test
+    public void doubleFunctionTest() throws Exception {
+        DoubleFunction<Integer> toInt = (d) -> (int) d;
+        assertThat(toInt.apply(1.1), is(1));
+    }
+    
+    @Test
+    public void unaryOperatorTest() throws Exception {
+        UnaryOperator<Integer> twice = (i) -> i * 2;
+        assertThat(twice.apply(10), is(20));
+    }
+    
+    @Test
+    public void binaryOperatorTest() throws Exception {
+        BinaryOperator<Integer> add = (a, b) -> a + b;
+        assertThat(add.apply(1, 2), is(3));
+    }
+    
+    @Test
+    public void supplierTest() throws Exception {
+        Supplier<Integer> ten = () -> 10;
+        assertThat(ten.get(), is(10));
+    }
+    
+    @Test
+    public void consumerTest() throws Exception {
+        Consumer<Integer> tester = (i) -> assertThat(i, is(10));
+        tester.accept(10);
+    }
+
+    @Test
+    public void predicateTest() throws Exception {
+        Predicate<Integer> isZero = (i) -> i == 0;
+        assertThat(isZero.test(10), is(false));
+        assertThat(isZero.test(0), is(true));
+    }
+}
