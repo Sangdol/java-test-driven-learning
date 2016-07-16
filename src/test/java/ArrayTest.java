eb79cde24103352225bb95ee948306f46fabb4c9
Hugh Lee <hammerha@gmail.com>
2016-07-16 23:43:53 +0900

    Add more array conversion tests

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,8 +1,8 @@
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -46,6 +46,9 @@ public class ArrayTest {
         matrix = new int[][]{{1, 2}, {10, 20}};
         assertThat(matrix[0][0], is(1));
         assertThat(matrix[1][1], is(20));
+
+        matrix = new int[2][];
+        assertThat(matrix[0], is(nullValue()));
     }
 
     @Test(expected = ClassCastException.class)
@@ -60,5 +63,20 @@ public class ArrayTest {
         Integer[] ints = Arrays.copyOf(objects, objects.length, Integer[].class);
         assertThat(ints[0], is(2));
         assertThat(ints[1], is(2));
+    }
+
+    @Test
+    public void listToArrayTest() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        Integer[] arr = list.toArray(new Integer[list.size()]);
+        assertThat(arr, is(new Integer[]{0, 1}));
+
+        List<Integer[]> arrList = new ArrayList<>();
+        arrList.add(new Integer[]{0});
+        arrList.add(new Integer[]{1});
+        Integer[][] matrix = arrList.toArray(new Integer[arrList.size()][]);
+        assertThat(matrix, is(new Integer[][]{new Integer[]{0}, new Integer[]{1}}));
     }
 }
