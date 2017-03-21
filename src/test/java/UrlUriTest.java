b510b454043faec297e36d0019fa450b3525753f
Sanghyun Lee <hammerha@gmail.com>
2017-03-21 18:42:38 +0100

    Add URL constructor tests

--- src/test/java/UrlUriTest.java
+++ src/test/java/UrlUriTest.java
@@ -1,12 +1,25 @@
 import org.junit.Test;
 
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class UrlUriTest {
+
+    @Test(expected = MalformedURLException.class)
+    public void testMalformedURLExceptionURL() throws MalformedURLException {
+        new URL("test");
+    }
+
+    @Test
+    public void testURL() throws MalformedURLException {
+        URL url = new URL("http://abc.com");
+        assertThat(url.getHost(), is("abc.com"));
+    }
 
     @Test
     public void testURI() {
