3b35ce061c81b8b77a988c916b3fb7afc494c6be
Sanghyun Lee <hammerha@gmail.com>
2018-12-11 17:48:37 +0100

    Add Mockito Runner tests

--- src/test/java/MockitoTest.java
+++ src/test/java/MockitoTest.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.LinkedList;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -11,11 +13,15 @@ import static org.mockito.Mockito.verify;
  * @author hugh
  */
 @SuppressWarnings("unchecked")
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTest {
 
+    // https://stackoverflow.com/questions/1652692/using-mockito-to-mock-classes-with-generic-parameters
+    @Mock
+    private LinkedList<String> mockedList;
+
     @Test
-    public void should_duplicated_verify_succeed() throws Exception {
-        LinkedList mockedList = mock(LinkedList.class);
+    public void should_duplicated_verify_succeed() {
         mockedList.add("a");
         verify(mockedList).add("a");
         verify(mockedList).add("a");
